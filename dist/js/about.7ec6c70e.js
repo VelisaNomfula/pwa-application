cfa7ed05c6927b03b2d002e08704ef49f4dfb69a
Velisa <vvnomfula@gmail.com>
2021-07-07 13:57:00 +0200

    Initial commit

---
+++ dist/js/about.7ec6c70e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about pa-6"},[n("h1",[t._v("About page")])])}],s=n("2877"),c={},l=Object(s["a"])(c,a,u,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.7ec6c70e.js.map
---
+++ dist/precache-manifest.7099753e78f2ab53f98c079c1ba5c071.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1d2fc1e8a6aff6a255",
+    "url": "/css/chunk-vendors.df5bc077.css"
+  },
+  {
+    "revision": "05c4d82eadc0ec029acfbde3ee3679a0",
+    "url": "/images/favicon-16x16.png"
+  },
+  {
+    "revision": "5578a9f04e3f3a07f5d998395e39e478",
+    "url": "/images/favicon-32x32.png"
+  },
+  {
+    "revision": "0eb97dfcf505cdbdc53295183688fbe9",
+    "url": "/images/icons8-linode-192.png"
+  },
+  {
+    "revision": "3ccdac55aa1f85dc1176cab94ddcccd7",
+    "url": "/images/icons8-linode-512.png"
+  },
+  {
+    "revision": "d15820cbf2e681b56f6d17f892e92859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35746173df652c954a44",
+    "url": "/js/about.7ec6c70e.js"
+  },
+  {
+    "revision": "3bbf9939dc9b2e7cecca",
+    "url": "/js/app.fe482553.js"
+  },
+  {
+    "revision": "7e1d2fc1e8a6aff6a255",
+    "url": "/js/chunk-vendors.d218f249.js"
+  },
+  {
+    "revision": "085af19299a09628403d3727cc14e295",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
